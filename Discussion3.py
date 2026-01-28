77855dc81c58aa74c8594cea25a800b71c1b3857
Tracy Ni <tracyni@TracydeMacBook-Pro-1252.local>
2026-01-28 11:42:32 -0500

    First Commit

--- Discussion3.py
+++ Discussion3.py
@@ -1,52 +1,43 @@
 import math
 
 class Rectangle():
-    # Create the constructor "__init__" method
 
-    # YOUR CODE HERE
-
-
-
-    # Create the "__str__" method
-
-    # YOUR CODE HERE
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
-
-    # Create the "area_calculator" method
-
-    # YOUR CODE HERE
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
 
-
-    # Create the "__eq__" method
-    # 
-    # Returns a boolean value
-
-    # YOUR CODE HERE
+    def area_calculator(self):
+        return self.width * self.height
 
 
-    
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height
+
 
 
 def main():
+
     r1 = Rectangle(10, 10)
-    # call the __str__ method
-    print(r1)
-    # call the area_calculator method
+    print("r1:", r1)
     print("Area:", r1.area_calculator())
-
-
-    r2 = Rectangle(10, 15)
-    print(r2)
-    print("Area:", r2.area_calculator())
-    # call the __eq__ method
-    print(r1 == r2)
     print()
 
-    # you can create additional rectangle objects to 
-    # test your code or learn more about how the class behaves
-    pass
+    r2 = Rectangle(10, 15)
+    print("r2:", r2)
+    print("Area:", r2.area_calculator())
+    print("Equal: r1 == r2?", r1 == r2)
+    print()
+
+    r3 = Rectangle(10, 15)
+    print("r3:", r3)
+    print("Area:", r3.area_calculator())
+    print("Equal: r2 == r3?", r2 == r3)
+
 
 if __name__ == "__main__":
     main()
